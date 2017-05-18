02d1deb2341669ae534d029624e35ed31f7f0bcd
Noturno <Noturno@192.168.7.177>
2017-05-18 19:09:43 -0300

    teste

--- Algoritimo/src/exercicios/numero4/itemd/Classe.java
+++ Algoritimo/src/exercicios/numero4/itemd/Classe.java
@@ -2,9 +2,7 @@ package exercicios.numero4.itemd;
 
 public class Classe {
 	public static void main (String[] args) {
-		if(1 == 1)  {
-			//System.out.println(“1 é igual a 1”);
-			
+		if(1 == 1)  {	
 			System.out.println("1 é igual a 1");
 			
 		}		
